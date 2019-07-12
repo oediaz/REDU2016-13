d0b1c11180a65320d63cc2d9882b4a8d29113a39
José Arellano <32889739+jlpepelucho@users.noreply.github.com>
2019-07-12 09:38:49 -0500

    Validacion_Redu

--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/MedicacionPK_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/MedicacionPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-10T12:28:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-12T07:20:39")
 @StaticMetamodel(MedicacionPK.class)
 public class MedicacionPK_ { 
 
--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Medicamento_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Medicamento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-06-10T12:28:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-12T07:20:39")
 @StaticMetamodel(Medicamento.class)
 public class Medicamento_ { 
 
