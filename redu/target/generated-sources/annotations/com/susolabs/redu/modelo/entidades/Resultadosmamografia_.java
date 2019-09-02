c36b1e90cc67025bca38c985d3feef091910ee67
José Arellano <32889739+jlpepelucho@users.noreply.github.com>
2019-09-02 07:11:36 -0500

    Ultima Libreria de PrimeFaces

--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Resultadosmamografia_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Resultadosmamografia_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-08-28T07:15:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-02T07:07:35")
 @StaticMetamodel(Resultadosmamografia.class)
 public class Resultadosmamografia_ { 
 
--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Tratamientocancermama_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Tratamientocancermama_.java
@@ -13,7 +13,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-08-28T07:15:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-02T07:07:35")
 @StaticMetamodel(Tratamientocancermama.class)
 public class Tratamientocancermama_ { 
 
