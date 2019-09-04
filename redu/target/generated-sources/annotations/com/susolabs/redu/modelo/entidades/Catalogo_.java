71d2b729bde794ce7f4339e8336f1ef9dafabce2
José Arellano <32889739+jlpepelucho@users.noreply.github.com>
2019-09-04 14:10:56 -0500

    Actualizaciones

--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Catalogo_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Catalogo_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-02T07:07:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-02T09:56:23")
 @StaticMetamodel(Catalogo.class)
 public class Catalogo_ { 
 
--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Resultadosmamografiaep_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Resultadosmamografiaep_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-02T07:07:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-02T09:56:23")
 @StaticMetamodel(Resultadosmamografiaep.class)
 public class Resultadosmamografiaep_ { 
 
