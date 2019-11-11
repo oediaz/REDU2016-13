c0b8002b6d5b5aa5467f66c0edbe29433e7a88c1
jlpepelucho <jlarellano@espe.edu.ec>
2019-11-11 11:23:43 -0500

    Ingreso de base template de base de conocimiento

--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Resultadosmamografiaep_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Resultadosmamografiaep_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-29T13:27:17")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-11T11:22:23")
 @StaticMetamodel(Resultadosmamografiaep.class)
 public class Resultadosmamografiaep_ { 
 
