6747c408539d80fe6d1212c5b3dc65066a6316e4
oediaz <40476730+oediaz@users.noreply.github.com>
2019-11-11 13:19:18 -0500

    Creacion de persistencia

--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Examen_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Examen_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-11T11:22:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-11T12:39:57")
 @StaticMetamodel(Examen.class)
 public class Examen_ { 
 
