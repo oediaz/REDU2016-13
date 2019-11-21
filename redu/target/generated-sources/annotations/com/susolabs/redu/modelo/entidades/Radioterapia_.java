6c4f10b4a4aed71a97620b1b80ac7a82c1da4c96
jlpepelucho <jlarellano@espe.edu.ec>
2019-11-21 12:26:04 -0500

    Intalación Python

--- redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Radioterapia_.java
+++ redu/target/generated-sources/annotations/com/susolabs/redu/modelo/entidades/Radioterapia_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-21T11:49:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-21T12:11:27")
 @StaticMetamodel(Radioterapia.class)
 public class Radioterapia_ { 
 
